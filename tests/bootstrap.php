e99df6bf8f475b2f03c1e66f97c8a40597fa86c7
Martijn van der Lee <martijn@vanderlee.com>
2026-08-02 09:55:25 +0200

    Load PHP 8.4 polyfills in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,11 @@ if (!class_exists('PHPUnit_Framework_TestCase', false)
     class_alias('PHPUnit\\Framework\\TestCase', 'PHPUnit_Framework_TestCase');
 }
 
+require_once __DIR__ . '/../functions/mb_polyfill_helpers.php';
 require_once __DIR__ . '/../functions/mb_explode.php';
 require_once __DIR__ . '/../functions/mb_trim.php';
+require_once __DIR__ . '/../functions/mb_ltrim.php';
+require_once __DIR__ . '/../functions/mb_rtrim.php';
 require_once __DIR__ . '/../functions/mb_ucfirst.php';
+require_once __DIR__ . '/../functions/mb_lcfirst.php';
 require_once __DIR__ . '/../functions/mb_ucwords.php';
