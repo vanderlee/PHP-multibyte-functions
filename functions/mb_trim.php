5232fb42333a60b22901b61b3c64be906766c93f
Martijn van der Lee <martijn@vanderlee.com>
2026-08-02 09:54:26 +0200

    Turn mb_trim into a PHP 8.4 polyfill

--- functions/mb_trim.php
+++ functions/mb_trim.php
@@ -1,11 +1,16 @@
 <?php
 
-/**
- * Strip whitespace (or other characters) from the beginning and end of a string
- * @param string $string
- * @return string
- */
-function mb_trim($string)
-{
-	return mb_ereg_replace('^\s*([\s\S]*?)\s*$', '\1', $string);
+if (!function_exists('mb_trim')) {
+    /**
+     * Strip whitespace or specified characters from both ends of a string.
+     *
+     * @param string      $string
+     * @param string|null $characters
+     * @param string|null $encoding
+     * @return string
+     */
+    function mb_trim($string, $characters = null, $encoding = null)
+    {
+        return _vanderlee_mb_trim_impl($string, $characters, $encoding, true, true);
+    }
 }
