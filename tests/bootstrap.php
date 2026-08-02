187103ed0063ac7f7885c647d017608a4287f890
Martijn van der Lee <martijn@vanderlee.com>
2026-08-02 09:46:36 +0200

    Make the test bootstrap portable

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
-	require_once 'functions/mb_explode.php';
-	require_once 'functions/mb_trim.php';
-	require_once 'functions/mb_ucfirst.php';
-	require_once 'functions/mb_ucwords.php';
+if (!class_exists('PHPUnit_Framework_TestCase', false)
+    && class_exists('PHPUnit\\Framework\\TestCase')) {
+    class_alias('PHPUnit\\Framework\\TestCase', 'PHPUnit_Framework_TestCase');
+}
 
+require_once __DIR__ . '/../functions/mb_explode.php';
+require_once __DIR__ . '/../functions/mb_trim.php';
+require_once __DIR__ . '/../functions/mb_ucfirst.php';
+require_once __DIR__ . '/../functions/mb_ucwords.php';
