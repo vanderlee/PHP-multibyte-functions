efbd7b8bddb98a09688455660542cd0d1badb550
Martijn van der Lee <martijn@vanderlee.com>
2026-08-02 09:54:50 +0200

    Turn mb_ucfirst into a PHP 8.4 polyfill

--- functions/mb_ucfirst.php
+++ functions/mb_ucfirst.php
@@ -1,17 +1,22 @@
 <?php
 
-/**
- * Make a string's first character uppercase
- * @param string $string
- * @param string $encoding
- * @return string
- */
-function mb_ucfirst($string, $encoding = null)
-{
-	if (!$encoding) {
-		$encoding = mb_internal_encoding();
-	}
+if (!function_exists('mb_ucfirst')) {
+    /**
+     * Make a string's first character title-cased.
+     *
+     * @param string      $string
+     * @param string|null $encoding
+     * @return string
+     */
+    function mb_ucfirst($string, $encoding = null)
+    {
+        $encoding = _vanderlee_mb_resolve_encoding($encoding);
 
-	return mb_strtoupper(mb_substr($string, 0, 1, $encoding), $encoding)
-			. mb_substr($string, 1, mb_strlen($string, $encoding) - 1, $encoding);
+        if ($string === '') {
+            return '';
+        }
+
+        return mb_convert_case(mb_substr($string, 0, 1, $encoding), MB_CASE_TITLE, $encoding)
+            . mb_substr($string, 1, mb_strlen($string, $encoding), $encoding);
+    }
 }
